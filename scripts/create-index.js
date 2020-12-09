72739e0544c2dda0de3650140f6e26d8864b1864
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2020-12-09 08:19:07 -0500

    Generate raw file links for github

--- scripts/create-index.js
+++ scripts/create-index.js
@@ -1,7 +1,16 @@
 const fs = require("fs");
-const files = fs.readdirSync(".");
-const indexObj = { files };
 
-console.log(files);
+const LOCAL_DIRNAME = "nodes";
+const GITHUB_USERNAME = "Michigander";
+const GITHUB_REPONAME = "fieldnotes";
+const GITHUB_BRANCHNAME = "master";
 
+const mapToGithubRawFileLink = (filename) =>
+  `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${GITHUB_REPONAME}/${GITHUB_BRANCHNAME}/${filename}`;
+
+console.log("Creating index...");
+const files = fs.readdirSync(LOCAL_DIRNAME);
+const githubFiles = files.map(mapToGithubRawFileLink);
+const indexObj = { githubFiles };
 fs.writeFileSync("./index.json", JSON.stringify(indexObj, null, 2));
+console.log(`${githubFiles.length} files indexed!`);
